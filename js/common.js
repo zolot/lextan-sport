eaa57f9c300ea2fec365de354186b0f034368ea3
zolot <zolot3437@gmail.com>
2015-09-17 20:25:39 +0300

    черновой вариант

--- js/common.js
+++ js/common.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
 	function wResize() {
-			$("#main").css("min-height", $(window).height());
+			$("#main, #about-us, #products, #polish, #contacts, #parket").css("min-height", $(window).height());
 	};
 	wResize();
 	$(window).resize(function() {
 		wResize()
 	});
 	
-	
+	$('.contacts-info-wrap').jScrollPane();
+
 });
